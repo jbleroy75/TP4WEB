f1b363bc255d654cbefc8de7384bc9f317cc1eda
Seraleme <seraleme.04@gmail.com>
2021-09-29 12:28:49 +0200

    Ex 7: create BaseButton with primary color

--- src/components/BaseButton.vue
+++ src/components/BaseButton.vue
@@ -21,8 +21,15 @@ button {
   margin: 5px;
 }
 
+button:hover {
+  background: #28b682;
+}
+
 button:disabled {
   background: #6dcca5;
   cursor: not-allowed;
+}
+button:disabled:hover {
+  background: #75dcb2;
 }
 </style>
