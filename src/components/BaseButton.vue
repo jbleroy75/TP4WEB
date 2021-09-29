3305e42024a58c8b5d2bdcd757c443782e75037b
Seraleme <seraleme.04@gmail.com>
2021-09-29 12:25:18 +0200

    Ex 7: create BaseButton with primary color

---
+++ src/components/BaseButton.vue
@@ -0,0 +1,28 @@
+<template>
+  <button>
+    <slot></slot>
+  </button>
+</template>
+
+<script>
+export default {
+  name: "BaseButton"
+}
+</script>
+
+<style scoped>
+button {
+  color: white;
+  background: #30cb92;
+  border-radius: 5px;
+  padding: 10px;
+  border: none;
+  font-weight: bold;
+  margin: 5px;
+}
+
+button:disabled {
+  background: #6dcca5;
+  cursor: not-allowed;
+}
+</style>
--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -1,10 +1,17 @@
 <template>
   <h1>HomePage</h1>
+  <base-button>My BaseButton</base-button>
+  <base-button disabled>My BaseButton disabled</base-button>
 </template>
 
 <script>
+import BaseButton from "@/components/BaseButton";
+
 export default {
   name: 'HomePage',
+  components: {
+    BaseButton,
+  }
 };
 </script>
 
