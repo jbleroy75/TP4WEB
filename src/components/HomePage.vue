e3a1a61cc2769d23e44b364a09ca0003ffbe3196
Seraleme <seraleme.04@gmail.com>
2021-09-29 13:41:40 +0200

    Ex 10: slowing down the button on click

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -1,9 +1,9 @@
 <template>
   <h1>HomePage</h1>
   <base-button color="primary">My BaseButton</base-button>
-  <base-button color="warn" disabled>My BaseButton</base-button>
+  <base-button color="warn" disabled>My BaseButton disabled</base-button>
   <base-button color="warn">My BaseButton</base-button>
-  <base-button color="danger" disabled>My BaseButton</base-button>
+  <base-button color="danger" disabled>My BaseButton disabled</base-button>
   <base-button color="danger">My BaseButton</base-button>
   <base-button disabled>My BaseButton disabled</base-button>
   <async-button>My AsyncButton</async-button>
